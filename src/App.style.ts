1f5818da3dd4d40a8df66866a41e8efddc4de3fe
jaejun <sjj050316@gmail.com>
2023-05-31 11:02:34 +0900

    Modify the style and put it in portfolio

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 
+export const Header = styled.div`
+font-size: xx-large;
+margin: 30px;
+`
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center
+  align-items: center;
 `;
 
 export const RowBox = styled.div`
   display: flex;
   align-items: center;
-
-  `;
+`;
 export const Input = styled.input`
   width: 500px;
   height: 32px;
@@ -20,29 +24,29 @@ export const Input = styled.input`
   outline: none;
   padding-left: 10px;
   background-color: rgb(233, 233, 233);
+`;
 
-    `;  
-
-export const Button = styled.button<{backgroundColor?: string }>`
-${({ backgroundColor}) => `backgroundColor: ${backgroundColor};`}
-    background-color:#0a0a23;
-    color: #fff;
-    border:none; 
-    border-radius:10px; 
-    width: 100px;
-    height: 30px;
-    
-    `;
+export const Button = styled.button<{ backgroundColor?: string }>`
+  ${({ backgroundColor }) => `backgroundColor: ${backgroundColor};`}
+  background-color:#0a0a23;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  width: 100px;
+  height: 30px;
+`;
 
 export const List = styled.ul`
+margin: 50px;
 `;
 
 export const Todo = styled.li`
-display: flex;
-justify-content: space-between;
-align-items: flex-end;
-width: 300PX;
-padding: 5px 0px 5px 5px;
-    margin-bottom: 5px;
-    border-bottom: 1px solid #dfdfdf;
-    font-size: 12px;`;
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: flex-end;
+  width: 300px;
+  padding: 20px 0px 5px 50px;
+  margin-bottom: 5px;
+  border-bottom: 1px solid #dfdfdf;
+  font-size: medium;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ChangeEvent, useEffect, KeyboardEvent } from "react";
 import "./App.css";
 
-import { Button, Container, RowBox, Input, List, Todo } from "./App.style";
+import { Button, Container, RowBox, Input, List, Todo, Header  } from "./App.style";
 // import * as S from './App.style';
 
 interface Todo {
@@ -11,36 +11,65 @@ interface Todo {
 }
 
 function App() {
+  // todolist data를 저장하는 state
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [todoName, setTodoName] = useState<string>('');
+
+  // todo를 추가하기 위해 적는 내용을 적는
+  const [todoName, setTodoName] = useState<string>("");
+
+  // 현재 수정중인 todo의 id를 담는 state
   const [editedId, setEditId] = useState<number | undefined>();
 
+  // 수정 버튼을 누른 뒤 나온 input의 값을 수정하는데 써야하기 때문에 state에 담아 놓는다.
+  const [editName, setEditName] = useState<string>("");
+
+  // todoName state를 input의 onCange에서 setState 해주는 함수
   const handleInputChange = ({
     target: { value },
   }: ChangeEvent<HTMLInputElement>) => {
     setTodoName(value);
   };
 
+  // todo를 만드는 input에서 enter를 눌렀을 때 todo를 추가해주는 함수
   const handlePressEnter = ({ key }: KeyboardEvent<HTMLInputElement>) => {
     if (key === "Enter") addTodo();
   };
+
+  // todo를 추가해주는 함수
   const addTodo = () => {
+    // todoName이 빈값이면 추가하지 않음 (이후 코드 실행 x)
     if (!todoName.trim()) return;
-    const count = Number(localStorage.getItem('count')) + 1;
-    localStorage.setItem('count', `${count}`);
+
+    // localStorage에서 count를 +1 해줌 (id로 사용하기 위해)
+    const count = Number(localStorage.getItem("count")) + 1;
+    localStorage.setItem("count", `${count}`);
+
+    // todos에 새로운 todo 추가
     setTodos((prevState) => [
       ...prevState,
       { id: prevState.length, name: todoName },
     ]);
+
+    // todoName 비워줌 (굳이 안비워도 됨)
     setTodoName("");
   };
 
+  // todos를 초기화 해주는 함수
   const resetTodos = () => {
     setTodos([]);
-    localStorage.setItem('count', '0');
-  }
-  
+    localStorage.setItem("count", "0");
+  };
+
+  // 수정할 todo의 내용을 input에서 설정해주는 함수
+  const handleEditName = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setEditName(value);
+  };
+
+  // 컴포넌트(페이지)가 "처음" 렌더리 ㅇ됐을 때 localStorage의 todos를 state에 넣어줌
   useEffect(() => {
+    // JSON.~ 함수는 에러가 나면 그냥 터져버리기 때문에 try/catch 로 감싸줌
     try {
       const parseData = JSON.parse(localStorage.getItem("todos") || "");
       setTodos(parseData);
@@ -48,6 +77,8 @@ function App() {
       console.log(error);
     }
   }, []);
+
+  // state todos가 변화 될 때 (추가, 삭제) localStorage에도 저장해줌
   useEffect(() => {
     console.log("mount");
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -55,6 +86,7 @@ function App() {
 
   return (
     <Container>
+      <Header>TodoList</Header>
       <RowBox>
         <Input
           placeholder="Todo를 입력해주세요"
@@ -65,34 +97,57 @@ function App() {
         <Button onClick={addTodo}>추가</Button>
         <Button onClick={resetTodos}>초기화</Button>
       </RowBox>
-        <List>
-          {todos.map(({ id, name }) => {
-            const isEdit: boolean = editedId === id;
-            const deleteTodo = () => {
-              setTodos(prevState => prevState.filter(v => v.id != id));
-            };
+      <List>
+        {todos.map(({ id, name }) => {
+          const isEdit: boolean = editedId === id;
 
-            const editTodo = () => {
-              setEditId(id);
-            };
+          const deleteTodo = () => {
+            setTodos((prevState) => prevState.filter((v) => v.id != id));
+          };
 
-            const cancelEdit = () => setEditId(undefined);
+          const toggleEditTodo = () => {
+            setEditId(isEdit ? undefined : id);
+          };
 
-            return (
-            <Todo key={id}>{isEdit ? <Input defaultValue={name}/>: name}
-            <Button onClick={deleteTodo}>삭제</Button>
-            {isEdit ? (
+          const editTodo = () => {
+            setTodos((prevState) =>
+              prevState.map((todo) =>
+                todo.id === id ? { ...todo, name: editName } : todo
+              )
+            );
+            toggleEditTodo();
+          };
+
+          return (
+            <Todo key={id}>
+              {isEdit ? (
+                <Input defaultValue={name} onChange={handleEditName} />
+              ) : (
+                name
+              )}
+
+              <Button onClick={deleteTodo}>삭제</Button>
+
+              {/* 1번째 방법 */}
+              <Button onClick={toggleEditTodo}>
+                {isEdit ? "취소" : "수정"}
+              </Button>
+              {isEdit && <Button onClick={editTodo}>저장</Button>}
+
+              {/* 2번째 방법 */}
+              {/* {isEdit ? (
               <>
-                <Button onClick={cancelEdit} backgroundColor="skyblue">취소</Button>
+                <Button onClick={toggleEditTodo} backgroundColor="skyblue">취소</Button>
                 <Button>저장</Button>
               </>
             ) : (
-            <Button onClick={editTodo}>수정</Button>
+            <Button onClick={toggleEditTodo}>수정</Button>
             )}
+             */}
             </Todo>
-            );
-          })}
-        </List>
+          );
+        })}
+      </List>
     </Container>
   );
 }
